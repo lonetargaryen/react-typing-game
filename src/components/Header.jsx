e0fd7fbc0f156f77923ae905c4aaec806915616e
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-01-16 19:41:33 +0530

    Add Home page and Header component. Add AnonymousPro font to index.css.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="component-header">
+            <h1>Practise your typing!</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+
+function Home() {
+    return (
+        <div className="app-home">
+            <div className="header-div">
+                <Header />
+            </div>
+        </div>
+    )
+}
+
+export default Home
