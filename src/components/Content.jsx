d081608c1485f73e8700bea57534caaf7b89f2c0
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-01-18 00:17:56 +0530

    Add Content component. Fix some responsiveness issues. Fix some flexbox alignment issues.

---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+import './Content.css'
+
+function Content() {
+    return (
+        <div className="component-content">
+
+            <p className="component-content">Click the start button to have a quote displayed and then type it out as fast as you can!</p>
+            <hr/>
+            <p className="component-quote">quote goes here</p>
+            <p className="component-status-message">Status message!</p>
+
+            <div className="input-form-div">
+                <button>Start!</button>
+                <input type="text" className="component-input-tag" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Content
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import Header from '../components/Header'
+import Content from '../components/Content'
+import './Home.css'
 
 function Home() {
     return (
         <div className="app-home">
             <div className="header-div">
                 <Header />
+            </div>
+            <div className="content-div">
+                <Content />
             </div>
         </div>
     )
