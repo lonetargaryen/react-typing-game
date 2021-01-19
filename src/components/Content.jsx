58ae1a0c43859e1f86a7dcb54502d950f53551f6
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-01-19 20:19:41 +0530

    Fix a bug where error state wouldn't be highlighted. Fix a few more button CSS bugs.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,21 +1,116 @@
+import React from 'react';
+import { render } from '@testing-library/react';
 import './Content.css'
 
-function Content() {
-    return (
-        <div className="component-content">
+class Content extends React.Component {
 
-            <p className="component-content">Click the start button to have a quote displayed and then type it out as fast as you can!</p>
-            <hr/>
-            <p className="component-quote">quote goes here</p>
-            <p className="component-status-message">Status message!</p>
+    componentDidMount() {
+        // all of our quotes
+        const quotes = [
+            'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+            'There is nothing more deceptive than an obvious fact.',
+            'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
+            'I never make exceptions. An exception disproves the rule.',
+            'What one man can invent another can discover.',
+            'Nothing clears up a case so much as stating it to another person.',
+            'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+        ];
+        // store the list of words and the index of the word the player is currently typing
+        let words = [];
+        let wordIndex = 0;
+        // the starting time
+        let startTime = Date.now();
+        // page elements
+        const quoteElement = document.getElementById('quote');
+        const messageElement = document.getElementById('message');
+        const typedValueElement = document.getElementById('typed-value');
 
-            <div className="input-form-div">
-                <button className="input-form-div"> <span className="input-form-div">Start!</span> </button>
-                <input type="text" className="component-input-tag" />
+        document.getElementById('start').addEventListener('click', () => {
+            // get a quote
+            const quoteIndex = Math.floor(Math.random() * quotes.length);
+            const quote = quotes[quoteIndex];
+            // Put the quote into an array of words
+            words = quote.split(' ');
+            // reset the word index for tracking
+            wordIndex = 0;
+        
+            // UI updates
+            // Create an array of span elements so we can set a class
+            const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+            // Convert into string and set as innerHTML on quote display
+            quoteElement.innerHTML = spanWords.join('');
+            // Highlight the first word
+            quoteElement.childNodes[0].className = 'highlight';
+            // Clear any prior messages
+            messageElement.innerText = '';
+        
+            // Setup the textbox
+            // Clear the textbox
+            typedValueElement.value = '';
+            // set focus
+            typedValueElement.focus();
+            // set the event handler
+        
+            // Start the timer
+            startTime = new Date().getTime();
+        });
+
+        typedValueElement.addEventListener('input', () => {
+            // Get the current word
+            const currentWord = words[wordIndex];
+            // get the current value
+            const typedValue = typedValueElement.value;
+        
+            if (typedValue === currentWord && wordIndex === words.length - 1) {
+                // end of sentence
+                // Display success
+                const elapsedTime = new Date().getTime() - startTime;
+                const message = `Nice! You finished in ${elapsedTime / 1000} seconds.`;
+                messageElement.innerText = message;
+            } 
+            else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+                // end of word
+                // clear the typedValueElement for the new word
+                typedValueElement.value = '';
+                // move to the next word
+                wordIndex++;
+                // reset the class name for all elements in quote
+                for (const wordElement of quoteElement.childNodes) {
+                    wordElement.className = '';
+                }
+                // highlight the new word
+                quoteElement.childNodes[wordIndex].className = 'highlight';
+            } 
+            else if (currentWord.startsWith(typedValue)) {
+                // currently correct
+                // highlight the next word
+                typedValueElement.className = 'input-form-div';
+            } 
+            else {
+                // error state
+                console.log("error state reached");
+                typedValueElement.className = 'error';
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="component-content">
+    
+                <p className="component-content">Click the start button to have a quote displayed and then type it out as fast as you can!</p>
+                <hr/>
+                <p className="component-quote" id="quote"></p>
+                <p className="component-status-message" id="message"></p>
+    
+                <div className="input-form-div">
+                    <button className="input-form-div" id="start"> <span className="input-form-div">Start!</span> </button>
+                    <input type="text" className="component-input-tag" id="typed-value"/>
+                </div>
+    
             </div>
-
-        </div>
-    )
+        );
+    }
 }
 
-export default Content
+export default Content;
